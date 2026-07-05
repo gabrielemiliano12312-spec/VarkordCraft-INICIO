425cf13e2fdfd74b044046979a39b94e0163b53d
gabrielemiliano12312-spec <gabrielemiliano12312@gmail.com>
2026-07-05 18:23:19 -0300

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "CONFIGURANDO",
+        name: "Con Mi Creador Nykolas_166",
         // Activity type number (0 = Playing).
         type: 0,
       },
